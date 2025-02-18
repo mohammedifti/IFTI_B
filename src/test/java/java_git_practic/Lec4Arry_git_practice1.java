67d4b7f22e6b4e8bc6c21da282701bc6bccf21a1
practice <test@test.com>
2025-02-18 16:29:55 -0500

    saving cone

---
+++ src/test/java/java_git_practic/Lec4Arry_git_practice1.java
@@ -0,0 +1,24 @@
+package java_git_practic;
+
+public class Lec4Arry_git_practice1 {
+	
+	public static void main(String[] args) {
+		//1.store 10 flower name using array
+		String [] flowers= {"rose","baly","cosmos","sunflower","lily","iris","poppy","leek","orchid","daisy"};
+		//2. change 4th flower with different flower
+		//flowers [0]="passion";
+		//flowers [1]="succulent";
+		//flowers [2]="Palash";
+		flowers [3]="rosebud";
+		//System.out.println(flowers [0]);
+		//System.out.println(flowers [1]);
+		//System.out.println(flowers [2]);
+	    System.out.println(flowers [3]);
+	    //3. find out length of array
+	    System.out.println(flowers.length);
+
+
+	}
+
+
+}
